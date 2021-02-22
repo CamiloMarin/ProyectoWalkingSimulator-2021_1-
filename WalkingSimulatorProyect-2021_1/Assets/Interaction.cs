e2230568a684c2f3ca0d0fc24feff6bc9bb8e7fb
Miloka <camilomv13@hotmail.com>
2021-02-21 19:51:44 -0500

    Mejora de movimiento y primer progreso para la mecanica de la luz
    -- Se cambió la configuración del personaje principal del movimiento (mejora)
    -- Se le asignó colores a los objetos para poderlos diferenciar mejor
    -- Se creó un boton de encendido y apagado de la luz (ES EL BOTON F)
    -- Se creo el script que da sensibilidad al mouse y a la rotacion
    -- Se creo un script que controla el cambio de ubicación de dos objetos al lanzar un raycast sobre ellos (PARA VER EL CAMBIO TIENE QUE ALEJAR EL POINT VIEW DE LOS CUBOS)

---
+++ WalkingSimulatorProyect-2021_1/Assets/Interaction.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    public GameObject playerCamera; // GameObj del objeto donde está la camara (DENTRO DEL PLAYER)
+    RaycastHit hit; // La variable que almacena el objeto al cual se le aplica el raycast
+
+    public GameObject Obj1;
+    public GameObject Obj2;
+    private int score = 1;
+    private int oldScore = 2;
+    public Vector3 tempPosition;
+
+
+
+    void Start()
+    {
+
+
+    }
+
+    public void Swap()
+    {
+        tempPosition = Obj1.transform.position;
+        Obj1.transform.position = Obj2.transform.position;
+        Obj2.transform.position = tempPosition;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // RAYCAST 
+
+        Debug.DrawRay(playerCamera.transform.position, playerCamera.transform.TransformDirection(Vector3.forward) * 8.0f, Color.red);
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.TransformDirection(Vector3.forward), out hit, 3.0f))
+        {
+            // hit.transform.gameObject.transform.position = new Vector3(Random.Range(-20, 20), 2, Random.Range(-20, 20));
+        
+            
+                if (hit.collider.tag.Equals("cambiante") && score != oldScore)
+
+                {         
+          
+                    Swap();
+                    score = oldScore;
+                    
+
+
+                }  else if (!hit.collider.tag.Equals("cambiante") && score == oldScore)
+
+
+                {
+                     score = 1;
+                }
+
+                  
+         
+
+
+
+
+        }
+           
+            
+
+
+
+            
+        
+    }
+}
---
+++ WalkingSimulatorProyect-2021_1/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f; // Velocidad de movimiento del personaje (PLAYER)
+    public float gravity = -9.81f; // Fuerza de gravedad que se aplica para el personaje (PLAYER)
+    public float jumpHeiht = 3f; // Fuerza de salto
+    public Transform groundCheck; // Transform de la clase a la cual esté anexada este constructor
+    public float groundDistance = 0.4f; //Distancia del suelo mientras se esta en el aire
+    public LayerMask groundMask; // mascara (LAYER) del groundmask (Identifica que objetos serán los que se puedan pisar)
+    public GameObject luz; // Clase del objeto que contiene a la luz
+    public int ContadorEncendidoLuz = 0; // contador para saber si la luz esta encendida o apagada
+    bool isGrounded; // Reviza si esta en el suelo, y si es verdadero, entonces la velocidad no se acumulará en Y (Además determina si se puede saltar o no) 
+    Vector3 velocity; // Velocidad que se le asigna al movimiento
+
+
+
+    private void Start()
+    {
+        
+    }
+
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    void Update()
+    {
+
+       
+
+
+
+
+        // Funcion del Boton de encendido y apagado de la luz
+
+        ButtonLight();
+
+
+        // crea una esfera en el objeto groundCheck. Esa esfera reviza si esta colisionando con el suelo
+        // Si la esfera está colisionando con ALGO en el suelo dentro de la esfera, entonces el bool grounded va a ser verdadero (true)
+
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal"); // Movimiento horizontal
+        float z = Input.GetAxis("Vertical"); // Movimiento Vertical
+
+        Vector3 move = transform.right * x + transform.forward * z; // Fuerza al movimiento en la dirección de x
+
+        controller.Move(move * speed * Time.deltaTime); // Funcíón que asigna el movimiento de un componente controlador en un objeto
+
+        // Salto
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeiht * -2 * gravity);
+        }
+
+        // "Smooth" en la caida
+        velocity.y += gravity * Time.deltaTime;
+        // "Smooth" en el mov en X
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+
+    void ButtonLight()
+        // Boton de la luz (Encender - Apagar) 
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+            {
+            
+
+            if(ContadorEncendidoLuz == 0)
+            {
+                luz.gameObject.SetActive(true);
+                ContadorEncendidoLuz = 1;
+            }
+            else if (ContadorEncendidoLuz == 1)
+            {
+                
+                luz.gameObject.SetActive(false);
+                ContadorEncendidoLuz -= 1;
+            }
+        }
+    }
+
+}

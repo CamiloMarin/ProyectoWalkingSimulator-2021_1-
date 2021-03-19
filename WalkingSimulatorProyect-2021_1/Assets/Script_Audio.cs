07f6a11bd21b5fbc1ad7b5ee692c60e904cb669d
Miloka <camilomv13@hotmail.com>
2021-03-19 06:27:34 -0500

    Se implementó el patrón de Singleton en un objeto de Audio_Manager
    -

---
+++ WalkingSimulatorProyect-2021_1/Assets/Script_Audio.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_Audio : MonoBehaviour
+{
+    public static Script_Audio _script_audio;
+    AudioSource _audioSource; 
+
+    
+     void Awake()
+    {
+        if (Script_Audio._script_audio = null)
+        {
+            // Se instancia por primera vez  (instancia única) (SINGLETON) 
+
+            Script_Audio._script_audio = this;
+            DontDestroyOnLoad(gameObject);
+            _audioSource = GetComponent<AudioSource>();
+
+        }
+        else
+        {
+            // si ya hay una instancia, entonces que destruya esta.
+
+            Destroy(gameObject);
+        }
+    }
+
+}

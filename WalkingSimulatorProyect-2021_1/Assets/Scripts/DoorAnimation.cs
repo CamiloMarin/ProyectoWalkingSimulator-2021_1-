fa6644b700f86e7b01e90be23866f83875d9c46f
iandrescamilo <adre_ss_15@hotmail.com>
2021-04-11 01:53:03 -0500

    Enemigos y animaciones de las puertas

---
+++ WalkingSimulatorProyect-2021_1/Assets/Scripts/DoorAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnimation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator door;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+        door.SetBool("Open", true);
+    }
+    else{
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            door.SetBool("Open", false);
+        }
+    }
+
+    }
+}
---
+++ WalkingSimulatorProyect-2021_1/Assets/Scripts/DoorAnimation3.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnimation3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator door;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+
+        door.SetBool("Open", true);
+    }
+    else{
+        if (Input.GetKeyUp(KeyCode.X))
+        {
+            door.SetBool("Open", false);
+        }
+    }
+
+    }
+}
